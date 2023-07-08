f34ec184c3fdcd71a3e1460dec713de98b2a7ec7
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-07-08 17:53:52 -0300

    remove page document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
+import Script from 'next/script'
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
@@ -16,6 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <head>
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-8436534269018722"
+          async={true}
+          strategy="beforeInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      </head>
       <body className={roboto.className}>{children}</body>
     </html>
   )
